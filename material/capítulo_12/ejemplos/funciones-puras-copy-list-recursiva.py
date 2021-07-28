f50085cff8fdaa2014c4a26bf9b5063e88ed3d62
Camilo Chacón Sartori <camilo.chacon@telefonica.com>
2021-07-28 22:45:00 +0200

    Update funciones-puras-copy-list-recursiva.py
    fix Bug

--- material/capítulo_12/ejemplos/funciones-puras-copy-list-recursiva.py
+++ material/capítulo_12/ejemplos/funciones-puras-copy-list-recursiva.py
@@ -1,9 +1,11 @@
 def copyList(lst):
     if not lst:
         return []
+    elif len(lst) == 1:
+        return [lst[0]]
     else:
         return copyList([lst[0]]) + copyList(lst[1:])
-a = [1,2,3]
+a = [1]
 b = copyList(a)
 print(a == b)
 # True
